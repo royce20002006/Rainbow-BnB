e3c65f49c298493cb6623b835e0210ba9f7921cb
Royce Calvin <royce20002006@yahoo.com>
2024-05-11 17:13:25 -0500

    fixed user to reviews relationship

---
+++ backend/utils/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+
+//sends a jwt cookie
+const setTokenCookie = (res, user) => {
+    //create the token
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+    };
+    const token = jwt.sign(
+        {data: safeUser },
+        secret,
+        {expiresIn: parseInt(expiresIn) } //604,800 seconds = 1 week
+    );
+
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    //set the token cookie
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000, //maxAge in milliseconds
+        httpOnly: true,
+        
+    })
+}
