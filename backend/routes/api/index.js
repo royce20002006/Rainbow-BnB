d51a3f806600a9aa58e52f542d7c58441d470e89
Royce Calvin <royce20002006@yahoo.com>
2024-05-13 23:38:17 -0500

    Add user login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,21 +1,19 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 const { restoreUser } = require('../../utils/auth');
+const { sequelize } = require('../../db/models');
 
 
 //restore user middleware
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
-
-
-
-
-
-
-
-
-
-
+router.post('/test', (req, res) => {
+    res.json({requestBody: req.body });
+});
 
 module.exports = router;
