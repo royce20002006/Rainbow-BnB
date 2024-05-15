1805d6d50120de9b9a932e87c4857d14065e5e34
Royce <royce20002006@yahoo.com>
2024-05-15 01:38:01 -0500

    hello

--- backend/db/seeders/20240515062514-demo-user.js
+++ backend/db/seeders/20240515062514-demo-user.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.insert([
+    await queryInterface.bulkInsert([
       {
         firstName: 'jan',
         lastName: 'Well',
