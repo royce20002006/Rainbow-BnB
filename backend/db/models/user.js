0433ece3fce4593c71afd915465239c7cab27727
Royce <royce20002006@yahoo.com>
2024-05-15 02:28:44 -0500

    validation error

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -48,7 +48,6 @@ module.exports = (sequelize, DataTypes) => {
         unique: true,
         validate: {
           len: [4, 30],
-          isAlphanumeric:true,
           isNotEmail(value) {
             if (Validator.isEmail(value)) {
               throw new Error("Cannot be an email.");
