75361f281b9fc71e727476e59867a3a602395c46
Royce Calvin <royce20002006@yahoo.com>
2024-05-15 23:41:46 -0500

    changes to json file

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,10 +1,9 @@
 module.exports = {
-    enviroment: process.env.NODE_ENV || 
-    'development',
+    environment: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 8000,
     dbFile: process.env.DB_FILE,
     jwtConfig: {
-        secret: process.env.JWT_SECRET,
-        expiresIn: process.env.JWT_EXPIRES_IN
+      secret: process.env.JWT_SECRET,
+      expiresIn: process.env.JWT_EXPIRES_IN
     }
-};
+  };
