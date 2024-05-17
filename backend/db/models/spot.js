7e56a20ecb03911a2606d91aa7c1dccddb8d681c
Royce Calvin <royce20002006@yahoo.com>
2024-05-17 00:19:51 -0500

    try to figure out error

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isAlphanumeric: true,
+        
         len: [5, 255]
       }
     },
