cfd1d974d44198da4fcd6d284fabc7392ea5fa16
Royce <royce20002006@yahoo.com>
2024-05-23 19:33:06 -0500

    get reviews for current user completed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users');
 const { restoreUser } = require('../../utils/auth');
 const { sequelize } = require('../../db/models');
 const  spotsRouter  = require('./spots');
+const reviewsRouter = require('./reviews');
 
 //restore user middleware
 router.use(restoreUser);
@@ -13,6 +14,8 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 router.use('/spots', spotsRouter)
+
+router.use('/reviews', reviewsRouter);
 
 router.post('/test', (req, res) => {
     res.json({requestBody: req.body });
---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,93 @@
+const express = require('express');
+
+
+const {  requireAuth } = require('../../utils/auth');
+const { Spot, User, Review, SpotImage, ReviewImage
+} = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { Op, Model, ValidationError } = require('sequelize');
+const reviewimage = require('../../db/models/reviewimage');
+
+const router = express.Router();
+
+
+router.get('/current', requireAuth, async (req, res, next) => {
+    try {
+        const { user } = req;
+        if (user) {
+            const reviews = await Review.findAll({
+                where: {
+                    userId: user.id
+                },
+                include: [{model: Spot, attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price']}]
+            });
+
+            let reviewFormatting = [];
+            if (reviews) {
+                
+                for (let review of reviews) {
+                    let url = ''
+                    let spot = await Spot.findByPk(review.spotId)
+                    let images = await spot.getSpotImages()
+                    let reviewImageArr = [];
+                    for (let image of images) {
+                        if (image.preview === true) {
+                            url += image.url
+                        }
+                    }
+                    let reviewImages = await review.getReviewImages()
+                    for (let image of reviewImages) {
+                        reviewImageArr.push({
+                            id: image.id,
+                            url: image.url
+                        })
+                        console.log(image)
+                    }
+                    reviewFormatting.push({
+                        id: review.id,
+                        userId: review.userId,
+                        spotId: review.spotId,
+                        review: review.review,
+                        stars: review.stars,
+                        createdAt: review.createdAt,
+                        updatedAt: review.updatedAt,
+                        User: {
+                            id: user.id,
+                            firstName: user.firstName,
+                            lastname: user.lastName
+                        },
+                        Spot: {
+                            id: spot.id,
+                            ownerId: spot.ownerId,
+                            address: spot.address,
+                            city: spot.city,
+                            state: spot.state,
+                            country: spot.country,
+                            lat: spot.lat,
+                            lng: spot.lng,
+                            name: spot.name,
+                            price: spot.price,
+                            previewImage: url
+                        },
+                        ReviewImages: reviewImageArr
+                        
+
+                        
+                            
+
+                    })
+                }
+                res.json({Reviews : reviewFormatting})
+            } 
+        }
+        
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+ module.exports = router
