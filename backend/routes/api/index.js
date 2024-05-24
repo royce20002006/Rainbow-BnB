685670f3738374848f77e3576dfb5a9282406043
Royce <royce20002006@yahoo.com>
2024-05-24 18:20:47 -0500

    endpoints for deleting images

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,8 @@ const { restoreUser } = require('../../utils/auth');
 const { sequelize } = require('../../db/models');
 const  spotsRouter  = require('./spots');
 const reviewsRouter = require('./reviews');
+const spotImages  = require('./spotsImages')
+const reviewImages = require('./reviewImages')
 
 //restore user middleware
 router.use(restoreUser);
@@ -16,6 +18,10 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter)
 
 router.use('/reviews', reviewsRouter);
+
+router.use('/spots-images', spotImages);
+
+router.use('/review-images', reviewImages);
 
 router.post('/test', (req, res) => {
     res.json({requestBody: req.body });
