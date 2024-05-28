bbeedad73edb8b84272ce79d91d7cb201f0112d5
Royce <royce20002006@yahoo.com>
2024-05-28 02:31:44 -0500

    postgress in dev

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,32 @@
 
 const config = require('./index');
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+const schema = db.schema
 
-module.exports = {
-  development: {
+const development = [
+  {
     storage: config.dbFile,
     dialect: "sqlite",
     seederStorage: "sequelize",
     logQueryParameters: true,
     typeValidation: true
   },
+  {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: 'sequelize'
+  },
+]
+
+module.exports = {
+  development: process.env.DB_DIALECT === 'postgres' ? development[1] : development[0],
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
