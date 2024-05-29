b95aec7075b0de429f066f0bdb013c60ac35f488
Royce Calvin <royce20002006@yahoo.com>
2024-05-28 20:49:58 -0500

    fixed parseint into number on bookings because it needs a decimal

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -51,10 +51,10 @@ router.get('/current', requireAuth, async (req, res, next) => {
                         city: spots.city,
                         state: spots.state,
                         country: spots.country,
-                        lat: parseInt(spots.lat),
-                        lng: parseInt(spots.lng),
+                        lat: Number(spots.lat),
+                        lng: Number(spots.lng),
                         name: spots.name,
-                        price: parseInt(spots.price),
+                        price: Number(spots.price),
                         previewImage: previewImageUrl
                     };
 
