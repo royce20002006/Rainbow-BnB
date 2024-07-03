681bfcc4bbd00c4fafd6f2e83f261bc0ad0b2569
Royce <royce20002006@yahoo.com>
2024-07-03 04:00:48 -0500

    added css to post a review button it however is not working currently

--- frontend/src/screens/Spot/components/Reviews/Reviews.jsx
+++ frontend/src/screens/Spot/components/Reviews/Reviews.jsx
@@ -7,7 +7,7 @@ import { getReviewsThunk } from "../../../../store/reviews";
 
 export default function Reviews() {
     const { id } = useParams();
-    
+
     const [isLoaded, setIsLoaded] = useState(false);
     const dispatch = useDispatch();
     const reviews = useSelector(state => state.reviewsState.allReviews)
@@ -16,26 +16,26 @@ export default function Reviews() {
 
     const [isOwner, setIsOwner] = useState(false);
     const [alreadyReviewed, setAlreadyReviewed] = useState(false)
-    
-    
 
-        useEffect(() => {
-            if (sessionUser) {
 
-                if (spot.ownerId === sessionUser.id) {
-                    setIsOwner(true);
-                }
-                for (let review of reviews) {
-                    if (review.userId === sessionUser.id) {
-                        console.log("reviewed")
-                        setAlreadyReviewed(true);
-                    }
+
+    useEffect(() => {
+        if (sessionUser) {
+
+            if (spot.ownerId === sessionUser.id) {
+                setIsOwner(true);
+            }
+            for (let review of reviews) {
+                if (review.userId === sessionUser.id) {
+                    console.log("reviewed")
+                    setAlreadyReviewed(true);
                 }
             }
-        },[reviews, spot.ownerId])
-    
-    
-    
+        }
+    }, [reviews, spot.ownerId])
+
+
+
 
     useEffect(() => {
         const getData = async () => {
@@ -56,27 +56,30 @@ export default function Reviews() {
 
     }
 
-    
-    
+
+
     return (
-        
-        <div>
+
+        <div className="reviewsStart">
+            <div className="ratingsWithButton">
             <div className="subheading">
                 <div><FaStar className="star" /> {spot.numReviews > 0 ? spot.avgStarRating : 'New'} · {spot.numReviews} reviews</div>
             </div>
             {
-                !sessionUser ? null : <div><button  disabled={isOwner}>Post Your Review</button></div>
+                !sessionUser ? null : <div classname='red' ><button disabled={isOwner}>Post Your Review</button></div>
             }
             {reviews.length === 0 && sessionUser ? <p>Be the first to post a review!</p> : null}
-            
+
+            </div>
+
             <div>
                 {reviews.map((review, idx) => (
-                    
-                    <div  className='review' key={`${idx}--${review.id}`}>
+
+                    <div className='review' key={`${idx}--${review.id}`}>
                         <h3>{review.User.firstName}</h3>
                         <div className="date">
-                        <div>{review.updatedAt.slice(5,7)}</div>
-                        <div>{review.updatedAt.split('-')[0]}</div>
+                            <div>{review.updatedAt.slice(5, 7)}</div>
+                            <div>{review.updatedAt.split('-')[0]}</div>
 
                         </div>
                         <p>{review.review}</p>
