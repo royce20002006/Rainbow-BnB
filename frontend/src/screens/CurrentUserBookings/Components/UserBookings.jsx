0318914be4c0addf687ac0c8b6b352ff59710e66
root <root@BLD.>
2025-02-22 19:02:44 -0600

    five

--- frontend/src/screens/CurrentUserBookings/Components/UserBookings.jsx
+++ frontend/src/screens/CurrentUserBookings/Components/UserBookings.jsx
@@ -53,7 +53,7 @@ export default function CurrentUserBookings() {
                                 <div className="bookings">
                                     <span className="booking-spot-name">{booking.Spot.name}</span>
                                     <span className="booking-start-date">{booking.startDate}</span>
-                                    <span className="booking-end-date">{booking.endDate}</span>
+                                    <span className="booking-end-dates">{booking.endDate}</span>
                                     <button>Edit</button>
                                     <button>Delete</button>
 
