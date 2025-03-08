62d635466a2324bee20a5af3dbe2014a83aae94a
root <root@BLD.>
2025-03-08 00:35:19 -0600

    two

--- frontend/src/screens/CurrentUserBookings/Components/UserBookings.jsx
+++ frontend/src/screens/CurrentUserBookings/Components/UserBookings.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom";
 
-import './Splash.css'
+import './UserBookings.css'
 import { getCurrentUserBookingsThunk } from "../../../store/bookings";
 
 
